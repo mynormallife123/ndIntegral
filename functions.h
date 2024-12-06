8b4bfe5b28c48c4ddd5d9533d05c022d963450f5
Huỳnh Nguyên Phát <hnphat0415@gmail.com>
2024-12-06 19:45:26 +0700

    Create functions.h

---
+++ functions.h
@@ -0,0 +1,14 @@
+// f(x, y, z):
+double func(const double x, const double y, const double z);
+
+// y1(x):
+double y1(const double x);
+
+// y2(x):
+double y2(const double x);
+
+// z1(x, y):
+double z1(const double x, const double y);
+
+// z2(x, y):
+double z2(const double x, const double y);
