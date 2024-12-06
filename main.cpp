6626ea183ee8e67a057623aa32d31ad8afbda0e7
Huỳnh Nguyên Phát <hnphat0415@gmail.com>
2024-12-06 19:41:46 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "quadrature3d.h"
+
+int main() {
+	int x1 = ;
+  int x2 = ;
+	double result = quad3d(func, x1, x2, y1, y2, z1, z2);
+	std::cout << "The result is: " << result;
+	return 0;
+}
