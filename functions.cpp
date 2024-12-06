9d346a39a86a4a8d67abe0e45dee000e4cde7171
Huỳnh Nguyên Phát <hnphat0415@gmail.com>
2024-12-06 19:44:27 +0700

    Create functions.cpp

---
+++ functions.cpp
@@ -0,0 +1,26 @@
+// Include of functions f(x, y, z), y1(x), y2(x), z1(x, y), z2(x, y):
+
+//f(x, y, z)
+double func(const double x, const double y, const double z) {
+	return x + y + z;
+}
+
+//y1(x)
+double y1(const double x) {
+	return 0.0;
+}
+
+//y2(x)
+double y2(const double x) {
+	return 1.0;
+}
+
+//z1(x, y)
+double z1(const double x, const double y) {
+	return 0.0;
+}
+
+//z2(x, y)
+double z2(const double x, const double y) {
+	return 1.0;
+}
